be7c4cbde6aa1618e8a3640551dfa0bd9130da03
George Jone <g.george.jone@gmail.com>
2019-05-20 19:22:54 -0700

    activate burger

--- script.js
+++ script.js
@@ -28,3 +28,14 @@ function topFunction() {
     behavior: 'smooth'
 }); // For Chrome, Firefox, IE and Opera
 }
+
+// The following code is based off a toggle menu by @Bradcomp
+// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+(function() {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#'+burger.dataset.target);
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
